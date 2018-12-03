f7c729d8887cb50cb588f879177264bc4273e159
itsalitee <ali@oneconcern.com>
2018-12-03 13:14:32 -0800

    trying bash script

---
+++ docker_resources/make_openfoam240.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+cd ~
+mkdir OpenFOAM
+cd OpenFOAM
+wget "http://downloads.sourceforge.net/foam/OpenFOAM-2.4.0.tgz?use_mirror=mesh" -O OpenFOAM-2.4.0.tgz
+wget "http://downloads.sourceforge.net/foam/ThirdParty-2.4.0.tgz?use_mirror=mesh" -O ThirdParty-2.4.0.tgz
+ 
+tar -xzf OpenFOAM-2.4.0.tgz 
+tar -xzf ThirdParty-2.4.0.tgz
+
+ln -s /usr/bin/mpicc.openmpi OpenFOAM-2.4.0/bin/mpicc
+ln -s /usr/bin/mpirun.openmpi OpenFOAM-2.4.0/bin/mpirun
+
+sed -i -e 's/^\(cgal_version=\).*/\1cgal-system/' OpenFOAM-2.4.0/etc/config/CGAL.sh
+
+export NUM_CPUS=$(cat /proc/cpuinfo | grep processor | wc -l | xargs)
+source $HOME/OpenFOAM/OpenFOAM-2.4.0/etc/bashrc WM_NCOMPPROCS=$NUM_CPUS
+
+echo "alias of240='source \$HOME/OpenFOAM/OpenFOAM-2.4.0/etc/bashrc $FOAM_SETTINGS'" >> $HOME/.bashrc
+
+wmSET $FOAM_SETTINGS
+
+# buid
+
+cd $WM_PROJECT_DIR
+ 
+#Change how the flex version is checked
+find src applications -name "*.L" -type f | xargs sed -i -e 's=\(YY\_FLEX\_SUBMINOR\_VERSION\)=YY_FLEX_MINOR_VERSION < 6 \&\& \1='
+
+mkdir -p $CGAL_ARCH_PATH
+mkdir -p $BOOST_ARCH_PATH
+
+cd $WM_PROJECT_DIR
+ 
+#Still better be certain that the correct Qt version is being used
+export QT_SELECT=qt4
+ 
+# This next command will take a while... somewhere between 30 minutes to 3-6 hours.
+./Allwmake > log.make 2>&1
+ 
+#Run it a second time for getting a summary of the installation
+./Allwmake > log.make 2>&1
