69a59c20a7f9254d44a28bd68230419c04f1ed62
itsalitee <ali@oneconcern.com>
2018-12-05 07:46:58 -0800

    clean code

--- docker_resources/openfoam240/make_lemos.sh
+++ docker_resources/openfoam240/make_lemos.sh
@@ -2,12 +2,6 @@
 
 set -exo pipefail
 
-# apt-get update \
-#     && apt-get install -yq curl make vim nano \
-#     && apt-get install -yq git-core build-essential cmake flex bison zlib1g-dev qt4-dev-tools libqt4-dev libqtwebkit-dev gnuplot \
-#     libreadline-dev libncurses5-dev libxt-dev libopenmpi-dev openmpi-bin libboost-system-dev libboost-thread-dev libgmp-dev \
-#     libmpfr-dev python python-dev libcgal-dev unzip libglu1-mesa-dev libqt4-opengl-dev libgsl-dev
-
 # getting the LEMOS files inside the docker
 unzip -q LEMOS-2.4.x-master.zip
 rm -f *.zip
