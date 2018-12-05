5120b0288ec706145411dcf032e232032df6b4e2
itsalitee <ali@oneconcern.com>
2018-12-05 07:45:27 -0800

    openfoam 2.4.0 with LEMOS

--- docker_resources/openfoam240/make_lemos.sh
+++ docker_resources/openfoam240/make_lemos.sh
@@ -2,14 +2,18 @@
 
 set -exo pipefail
 
+# apt-get update \
+#     && apt-get install -yq curl make vim nano \
+#     && apt-get install -yq git-core build-essential cmake flex bison zlib1g-dev qt4-dev-tools libqt4-dev libqtwebkit-dev gnuplot \
+#     libreadline-dev libncurses5-dev libxt-dev libopenmpi-dev openmpi-bin libboost-system-dev libboost-thread-dev libgmp-dev \
+#     libmpfr-dev python python-dev libcgal-dev unzip libglu1-mesa-dev libqt4-opengl-dev libgsl-dev
+
 # getting the LEMOS files inside the docker
 unzip -q LEMOS-2.4.x-master.zip
 rm -f *.zip
 mv LEMOS-2.4.x-master LEMOS-2.4.x
 
-# echo "source \$HOME/OpenFOAM/OpenFOAM-2.4.0/etc/bashrc" >> $HOME/.bashrc
-# source ~/.bashrc
-
+source ~/.bashrc
 mv ./LEMOS-2.4.x $FOAM_SRC/
 echo "source \$FOAM_SRC/LEMOS-2.4.x/bashrc" >> $HOME/.bashrc
 source ~/.bashrc
@@ -18,3 +22,4 @@ cd $FOAM_SRC/LEMOS-2.4.x
 chmod a+x ./applyPatches ./Allwmake
 ./applyPatches
 ./Allwmake
+
